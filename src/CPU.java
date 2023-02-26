9179379cc1e6ee6e5a1fcacd934c544a5ff5b2a1
MihailGustiuc <mihail.gustiuc95@gmail.com>
2023-02-26 19:27:02 +0200

    Initial Commit1

--- src/CPU.java
+++ src/CPU.java
@@ -1,14 +1,5 @@
 public class CPU {
     CPU cpu = new CPU();
-//    private Processor processor;
-//    private RAM ram;
-//
-//    public CPU() {
-//        processor = new Processor();
-//        ram = new RAM();
-//    }
-
-    // Processor inner class
     private class Processor {
         Processor processor = new Processor();
 
